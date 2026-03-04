ab5b5008ce39fb075b1eee1acc51e8040406d128
Adil Khan <adilk3529@gmail.com>
2026-03-04 21:51:52 +0530

    loaded and split it to chunks

---
+++ ingest.py
@@ -0,0 +1,24 @@
+from langchain_community.document_loaders import PyPDFLoader,TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def load_and_chunk(file_path: str):
+    #Load the documnets and split it into chunks
+
+    if file_path.endswith(".pdf"):
+        loader = PyPDFLoader(file_path)
+
+    else:
+        loader = TextLoader(file_path)
+
+    documents = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        sepeartors = ["\n\n", "\n", " ", ""]
+
+    )
+
+    chunks = splitter.split_documents(documents)
+    print(f"✅ Loaded {len(documents)} pages → {len(chunks)} chunks")
+    return chunks
